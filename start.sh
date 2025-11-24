df1394a86d6d404b6dcd6c18432377146720b71c
phornnato <natophorn@gmail.com>
2025-11-24 13:32:22 +0700

    new updat2e

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Ensure PORT has a default value for nginx listen
+PORT=${PORT:-80}
+export PORT
 # Substitute PORT into nginx config if template exists
 if [ -f /etc/nginx/conf.d/default.conf.template ]; then
   envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
